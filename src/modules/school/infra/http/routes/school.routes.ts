0c9c8f15a0e46e0e8a0a20ccaa73f2c02cf8e595
Israel Batista <israelbatiista19@gmail.com>
2021-01-04 13:54:56 -0300

    Aplicando permissão para criação de escola

--- src/modules/school/infra/http/routes/school.routes.ts
+++ src/modules/school/infra/http/routes/school.routes.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 
 import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticaded from '@modules/users/infra/middlewares/ensureAuthenticated';
-import checkAccess from '@modules/users/infra/middlewares/checkRoleAccess';
 import SchoolController from '../controllers/SchoolController';
 import ListSchoolUserController from '../controllers/ListSchoolUserController';
 
@@ -10,12 +9,6 @@ const schoolRouter = Router();
 const schoolController = new SchoolController();
 const listSchoolUserController = new ListSchoolUserController();
 schoolRouter.use(ensureAuthenticaded);
-
-schoolRouter.use((req, response, next) => {
-  checkAccess(req, response, next, 'Master').catch(error => {
-    response.json({ error: { message: error.message } });
-  });
-});
 
 schoolRouter.post(
   '/',
--- src/modules/school/services/CreateSchoolService.ts
+++ src/modules/school/services/CreateSchoolService.ts
@@ -1,3 +1,5 @@
+import IUserRepository from '@modules/users/repositories/IUserRepository';
+import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
 import School from '../infra/typeorm/entities/School';
 import ISchoolRepository from '../repositories/ISchoolRepository';
@@ -12,9 +14,21 @@ class CrearteSchoolService {
   constructor(
     @inject('SchoolRepository')
     private schoolRepository: ISchoolRepository,
+
+    @inject('UserRepository')
+    private userRepository: IUserRepository,
   ) {}
 
   public async execute({ name, user_id }: IRequest): Promise<School> {
+    const canUserAccessResource = await this.userRepository.isProfile(
+      user_id,
+      'Master',
+    );
+
+    if (!canUserAccessResource) {
+      throw new AppError('Usuário não pode acessar esse recurso');
+    }
+
     const school = await this.schoolRepository.create({
       name,
       user_id,
